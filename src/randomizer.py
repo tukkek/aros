a379e2d5335669f5aaf4f76dd32dbc47a9943759
Alex Henry <tukkek@gmail.com>
2022-08-24 13:31:48 -0300

    Predictable debug artifact filenames

--- src/randomizer.py
+++ src/randomizer.py
@@ -7,7 +7,7 @@ import progression,solver,items,shutil,rooms,args,spoilers,shop,rpg,monsters,rom
 AREAS=list(progression.areas.keys())
 REWARDS=[progression.BACKDASH[0],progression.GLIDE[0],progression.JUMP[0],progression.SLIDE[0],progression.WATERWALK[0],progression.SINK[0],progression.FLIGHT[0]] #TODO can add good candidates 'Kicker skeleton','Hippogryph','Galamoth', "Black Panther' but is it too much for new players to realize they cleared an area? maybe if removed from elsewhere in the game, arule of "basic move = level cleared" could be a good guideline
 FILLER=[progression.GLIDE[0],progression.WATERWALK[0],progression.SINK[0],progression.FLIGHT[0]] #TODO every area having a progression item would be essential for routing - this should be easy by placing duplicates of souls, as the game should allow it. (ideally would place multiple relics too, if game allows).
-FILENAME=f'aros.{args.seed}'
+FILENAME='aros' if args.debug else f'aros.{args.seed}'
 
 rpg.setup()
 while len(REWARDS)<len(AREAS):
